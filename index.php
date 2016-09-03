edc0846b953d7c359563d899eb8f9bd0eeebaedc
Alexander Bassov <alexander.bassov@gmx.de>
2016-09-03 20:14:44 +0200

    clean up

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 /*
@@ -14,23 +13,4 @@
 		->Init()
 		->getService("router")
 		->parseRequest();
-	//
-=======
-<?php
-
-/*
-*	.. and the story begins ..
-*
-*	Author: Alexander Bassov - 29.08.2016
-*/
-
-	//_________________________________________________________________________________________________________
-	// startup
-	require_once (__DIR__ . "/bootstrap.php");
-	$instance = \Infinex\Core\Bootstrap\Bootstrap::getInstance()
-		->Init()
-		->getService("router")
-		->parseRequest();
-	//
->>>>>>> 73952a55108ef9ecdf3a59b13e8183cc83289640
 	//_________________________________________________________________________________________________________
